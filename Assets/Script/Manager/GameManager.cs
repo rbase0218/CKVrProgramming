eee70828a3fc7d706a01cfe45748454c36291f82
rbase0218 <rbase@naver.com>
2022-12-08 19:03:48 +0900

    [chore] 프로젝트 초기 세팅

---
+++ Assets/Script/Manager/GameManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager>
+{
+    private int _gameState;
+
+    void Awake()
+    {
+
+    }
+}
